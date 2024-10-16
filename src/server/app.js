8f07e7517319b9fe1656b3af98679ea9b73f2ecc
Marcella Chaves <marcellafccosta@gmail.com>
2024-10-16 11:32:04 -0300

    bd

---
+++ src/server/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Algo deu errado!');
+});
+
+export default app;
